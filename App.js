bed228957abfaba2fbbdcf4b8ec812825c157d17
sahilwins <89785309+sahilwins@users.noreply.github.com>
2021-11-29 12:22:51 -0800

    ww

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View,SafeAreaView} from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Airanbbbb</Text>
+      <Text>jjjj</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ ios/Pods/Target Support Files/Pods-Airanb-AiranbTests/Pods-Airanb-AiranbTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Airanb_AiranbTests : NSObject
+@end
+@implementation PodsDummy_Pods_Airanb_AiranbTests
+@end
